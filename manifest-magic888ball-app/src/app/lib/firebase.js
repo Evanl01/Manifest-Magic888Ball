e826a0dc82ad3e65d86b278f23a2b11a7204f462
David Guo <davidguo@usc.edu>
2025-02-14 16:11:52 -0800

    made apikey secret and accessed through env variable

--- manifest-magic888ball-app/src/app/lib/firebase.js
+++ manifest-magic888ball-app/src/app/lib/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore, collection, addDoc, serverTimestamp, getDocs } from "fire
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCaI_dxhP03X7r-STXZGDILyjIrC5oBKKY",
-  authDomain: "manifest-magic.firebaseapp.com",
-  projectId: "manifest-magic",
-  storageBucket: "manifest-magic.appspot.com",
-  messagingSenderId: "695559784271",
-  appId: "1:695559784271:web:e7f1a0ebd8ed40b954a73a"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase app
@@ -20,25 +20,27 @@ const db = getFirestore(app);
 
 // Function to add an entry to Firestore
 export const addEntry = async (question, response) => {
-  try {
-    await addDoc(collection(db, "entries"), {
-      question,
-      response,
-      date: serverTimestamp(), // Automatically store timestamp
-    });
-    console.log("Entry added successfully!");
-  } catch (error) {
-    console.error("Error adding entry:", error);
-  }
+    try {
+        console.log('API Key:', process.env.NEXT_PUBLIC_FIREBASE_API_KEY); // Check if it's printing correctly
+
+        await addDoc(collection(db, "entries"), {
+            question,
+            response,
+            date: serverTimestamp(), // Automatically store timestamp
+        });
+        console.log("Entry added successfully!");
+    } catch (error) {
+        console.error("Error adding entry:", error);
+    }
 };
 
 // Function to get all entries from Firestore
 export const getEntries = async () => {
-  const querySnapshot = await getDocs(collection(db, "entries"));
-  return querySnapshot.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data()
-  }));
+    const querySnapshot = await getDocs(collection(db, "entries"));
+    return querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
 };
 
 export { db };
