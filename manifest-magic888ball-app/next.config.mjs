49649d61e27865928202f04a179c8fdace22ef31
dcho24 <jaehcho@usc.edu>
2025-04-06 17:45:03 -0700

    fix rendering

--- manifest-magic888ball-app/next.config.mjs
+++ manifest-magic888ball-app/next.config.mjs
@@ -4,5 +4,8 @@ const nextConfig = {};
 export default {
     // comment out the line below when testing with npm run dev
     output: 'export', // Ensures static export for Firebase Hosting
+    images: {
+        unoptimized: true, 
+    },
     trailingSlash: true, // Helps with Firebase Hosting routing
 };
